ee771af6183f3b92ba60f582751727cc88733f89
Fidel Rodriguez <f1d31@outlook.com.br>
2023-03-08 12:26:49 -0300

    qr-project

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QrReader } from 'react-qr-reader';
+
+import './index.css';
+
+function App() {
+  const handleScan = (data: string | null) => {
+    alert(data);
+  }
+
+  const handleError = (err: any) => {
+    console.error(err);
+  }
+
+  return (
+    <div>
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '100%' }}
+      />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
